cf45edf4e775879824b5d3ddb635014ce9aea254
jotebu <andy@zwoyczyk.de>
2026-09-07 03:07:02 +0200

    Statischen Statusfilter aus Einstellungen entfernen

--- ZabbixWidget/stoermeldejournal/includes/WidgetForm.php
+++ ZabbixWidget/stoermeldejournal/includes/WidgetForm.php
@@ -5,7 +5,6 @@ namespace Modules\Stoermeldejournal\Includes;
 use Zabbix\Widgets\CWidgetForm;
 use Zabbix\Widgets\Fields\CWidgetFieldIntegerBox;
 use Zabbix\Widgets\Fields\CWidgetFieldMultiSelectGroup;
-use Zabbix\Widgets\Fields\CWidgetFieldSelect;
 
 class WidgetForm extends CWidgetForm {
 
@@ -14,13 +13,6 @@ class WidgetForm extends CWidgetForm {
 			->addField(new CWidgetFieldMultiSelectGroup('groupids', 'Hostgruppen'))
 			->addField(
 				(new CWidgetFieldIntegerBox('history_days', 'Verlauf in Tagen', 1, 3650))->setDefault(30)
-			)
-			->addField(
-				(new CWidgetFieldSelect('status_filter', 'Status', [
-					0 => 'Alle Meldungen',
-					1 => 'Aktive Meldungen',
-					2 => 'Behobene Meldungen'
-				]))->setDefault(0)
 			)
 			->addField(
 				(new CWidgetFieldIntegerBox('show_lines', 'Angezeigte Zeilen', 10, 1000))->setDefault(50)
