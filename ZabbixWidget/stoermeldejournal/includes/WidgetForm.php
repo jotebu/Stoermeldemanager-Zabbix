c017f58c329931210e6795742623518944c8df48
jotebu <andy@zwoyczyk.de>
2026-09-07 02:47:55 +0200

    Widget-Einstellungen vollständig eindeutschen

--- ZabbixWidget/stoermeldejournal/includes/WidgetForm.php
+++ ZabbixWidget/stoermeldejournal/includes/WidgetForm.php
@@ -11,19 +11,19 @@ class WidgetForm extends CWidgetForm {
 
 	public function addFields(): self {
 		return $this
-			->addField(new CWidgetFieldMultiSelectGroup('groupids', _('Host groups')))
+			->addField(new CWidgetFieldMultiSelectGroup('groupids', 'Hostgruppen'))
 			->addField(
-				(new CWidgetFieldIntegerBox('history_days', _('History in days'), 1, 3650))->setDefault(30)
+				(new CWidgetFieldIntegerBox('history_days', 'Verlauf in Tagen', 1, 3650))->setDefault(30)
 			)
 			->addField(
-				(new CWidgetFieldSelect('status_filter', _('Status'), [
-					0 => _('All'),
-					1 => _('Active'),
-					2 => _('Resolved')
+				(new CWidgetFieldSelect('status_filter', 'Status', [
+					0 => 'Alle Meldungen',
+					1 => 'Aktive Meldungen',
+					2 => 'Behobene Meldungen'
 				]))->setDefault(0)
 			)
 			->addField(
-				(new CWidgetFieldIntegerBox('show_lines', _('Show lines'), 10, 1000))->setDefault(50)
+				(new CWidgetFieldIntegerBox('show_lines', 'Angezeigte Zeilen', 10, 1000))->setDefault(50)
 			);
 	}
 }
