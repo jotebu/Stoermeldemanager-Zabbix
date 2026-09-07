6616ba81e516664e7a5d285b8e23ee6ffcf004ed
jotebu <andy@zwoyczyk.de>
2026-09-07 02:35:51 +0200

    Quittierungsoption im Dialog zuverlässig vorauswählen

--- ZabbixWidget/stoermeldejournal/assets/js/class.widget.js
+++ ZabbixWidget/stoermeldejournal/assets/js/class.widget.js
@@ -22,4 +22,32 @@ class CWidgetStoermeldejournal extends CWidget {
 	onDeactivate() {
 		$.unsubscribe('acknowledge.create', this._events.acknowledgeCreated);
 	}
+
+	setContents(response) {
+		super.setContents(response);
+
+		for (const button of this._contents.querySelectorAll('.smj-ack-button')) {
+			button.addEventListener('click', event => {
+				event.preventDefault();
+				this.#openAcknowledgeDialog(button);
+			});
+		}
+	}
+
+	#openAcknowledgeDialog(button) {
+		const acknowledge_action = Number(button.dataset.ackAction);
+		const overlay = acknowledgePopUp({
+			eventids: [button.dataset.eventid],
+			acknowledge_problem: acknowledge_action
+		}, button);
+
+		overlay.xhr.then(() => {
+			const checkbox = overlay.$dialogue[0].querySelector('[name="acknowledge_problem"]');
+
+			if (checkbox !== null && !checkbox.checked) {
+				checkbox.checked = true;
+				checkbox.dispatchEvent(new Event('change', {bubbles: true}));
+			}
+		});
+	}
 }
